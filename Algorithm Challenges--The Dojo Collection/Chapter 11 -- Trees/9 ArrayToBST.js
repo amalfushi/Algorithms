9962884bae702f611a0999c0c112708a377a244e
amalfushi <amalfushi@gmail.com>
2018-03-25 14:59:42 -0700

    Ordered Array to BST

---
+++ Algorithm Challenges--The Dojo Collection/Chapter 11 -- Trees/9 ArrayToBST.js
@@ -0,0 +1,19 @@
+const link = require("./_BST.js");
+
+//given an array that is sorted in ascending order, return a BST object that is height balanced.
+
+
+function arrayToBST(arr, start = 0, end = arr.length - 1) {
+    if (start > end) return null;
+    let mid = Math.round((start + end) / 2)
+    let node = new link.BTNode();
+    node.value = arr[mid];
+
+    node.left = arrayToBST(arr, start, mid - 1);
+    node.right = arrayToBST(arr, mid + 1, end);
+    return node;
+}
+
+let tree = new link.BST();
+tree.root = arrayToBST([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+console.log(tree);
