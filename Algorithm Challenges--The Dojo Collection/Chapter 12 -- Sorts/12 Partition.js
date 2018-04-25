cff37805b9cbb66528048d8e58ef86d039a281fb
amalfushi <amalfushi@gmail.com>
2018-04-24 22:54:49 -0700

    Partition an array on the first index

---
+++ Algorithm Challenges--The Dojo Collection/Chapter 12 -- Sorts/12 Partition.js
@@ -0,0 +1,32 @@
+// Partition an unsorted array in-place. Use arr[0] as pivot value, return idx of the pivot
+
+// [5, 4, 9, 2, 5, 3] => [4, 2, 3, 5, 9, 5] returns 4
+// 4, 5, 9, 2, 5, 3
+// 4, 5, 9, 2, 5, 3
+// 4, 2, 5, 9, 5, 3
+// 4, 2, 3, 5, 9, 5
+
+// this request seems janky as heck.
+
+function partitionArray(arr) {
+    let part = arr[0];
+    let greaterThan = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] >= part) {
+            // splice splits an array on either side of the index provide (+- the number of elements to remove)returns them in a new array  while changing the original array to the two split portions
+            greaterThan.push(arr.splice(i, 1)[0]);
+            i--;
+        }
+    }
+
+    arr = arr.concat(greaterThan);
+    return arr;
+}
+
+function swap(arr, firstIndex, secondIndex) {
+    let temp = arr[firstIndex];
+    arr[firstIndex] = arr[secondIndex];
+    arr[secondIndex] = temp;
+}
+
+console.log(partitionArray([5, 4, 9, 2, 5, 3]));
